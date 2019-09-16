2ff7bb361bdc3f98fd9c61fe5a0a39e8a682c585
healthypackrat <healthypackrat@gmail.com>
2019-09-16 22:16:46 +0900

    Updates

---
+++ precache-manifest.136239ec6089c8c266050ba311be5818.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5fbcbf948034b25f93298a40c92d653",
+    "url": "/redux-real-world-example/index.html"
+  },
+  {
+    "revision": "356661e623815785b7fc",
+    "url": "/redux-real-world-example/static/js/2.2bf669c5.chunk.js"
+  },
+  {
+    "revision": "1dafa22e1f5839202d71",
+    "url": "/redux-real-world-example/static/js/main.ba49ead7.chunk.js"
+  },
+  {
+    "revision": "800b99878b53cf726949",
+    "url": "/redux-real-world-example/static/js/runtime~main.04a6dccd.js"
+  }
+]);
